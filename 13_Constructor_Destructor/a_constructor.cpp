4f579a42338063a751f5347c516f80b1b4b07c58
Vijaya Malla <mallabiju@gmail.com>
2022-12-21 07:59:03 +0545

    Constructor

---
+++ 13_Constructor_Destructor/a_constructor.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+
+    // declaring constructor
+    Student()
+    {
+        roll = 12;
+    }
+};
+
+int main()
+{
+    Student s1;
+    cout << "The roll number is = " << s1.roll;
+    return 0;
+}
